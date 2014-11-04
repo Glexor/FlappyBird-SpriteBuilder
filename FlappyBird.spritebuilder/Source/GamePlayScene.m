806fa3c608ebf58f39119c88673482e7ea657e17
Kyle Winkleman <kylebwinkleman@KeplersBrain.local>
2014-11-03 22:40:50 -0600

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -10,11 +10,25 @@
     character = (Character*)[CCBReader load:@"Character"];
     [physicsNode addChild:character];
     [self addObstacle];
+    timeSinceObstacle = 0.0f;
 }
 
 -(void)update:(CCTime)delta
 {
     // put update code here
+    //Increment time since last obstacle added
+    timeSinceObstacle += delta;
+    
+    //Check to see if two seconds have passed
+    if (timeSinceObstacle > 2.0f)
+    {
+        //add new obstacle
+        [self addObstacle];
+        
+        //Reset timeSinceObstacle
+        timeSinceObstacle = 0.0f;
+    }
+    
 }
 
 // put new methods here
